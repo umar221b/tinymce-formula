a1ea614349196e13afd70c513279082f6984e8f7
Umar Alkafaween <omar.ka923@gmail.com>
2022-06-15 11:47:45 +0300

    Add most of the Arabic translations

---
+++ src/translations/ar.js
@@ -0,0 +1,47 @@
+/**
+ * Created by panos on 10/16/15.
+ */
+(function() {
+  'use strict'
+
+  window.trans = {
+    'symbols': 'رموز',
+    'basic-math': 'رياضيات',
+    'greek-letters': 'أحرف يونانية',
+    'letter-symbols': 'رموز أخرى',
+    'relations': 'علاقات',
+    'sets': 'مجموعات',
+    'geometry': 'هندسة رياضية',
+    'radial-script-fraction': 'جذور، كسور، سكريبتس',
+    'fractions': 'الكسور',
+    'roots': 'الجذور',
+    'superscripts-subscripts': 'رفع، خفض',
+    'spaces': 'فراغات',
+    'integrals-limit': 'تكامل، نهايات',
+    'integrals': 'تكامل',
+    'differentials': 'اشتقاق',
+    'limits': 'نهايات',
+    'gradient-operators': 'Gradient operators',
+    'functions': 'فنكشنز',
+    'big-operators': 'معاملات كبيرة',
+    'summation': 'الجمع',
+    'product': 'الضرب',
+    'union': 'الاتحاد',
+    'matrices': 'المصفوفات',
+    'tables': 'الجداول',
+    'rows-columns': 'صفوف، أعمدة',
+    'equation-parts': 'أجزاء معادلات',
+    'decorations': 'Decorations',
+    'parenthesis': 'أقواس',
+    'accents': 'Accents',
+    'encloses': 'Encloses',
+    'arrows': 'أسهم',
+    'arrow-symbols': 'أسهم',
+    'ellipsis': 'بيضوي',
+    'arrow-scripts': 'Arrows with scripts',
+    'result': 'النتيجة',
+    'rows': 'أسطر',
+    'columns': 'أعمدة',
+    'ok': 'موافق'
+  }
+}())
